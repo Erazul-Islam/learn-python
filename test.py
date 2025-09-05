5a787baf1aeef6ccc01653c954b4eed93135e8e8
Erazul Islam <erazul89@gmail.com>
2025-09-06 01:32:35 +0600

    initialized python

---
+++ test.py
@@ -0,0 +1,16 @@
+name = "Taosif"
+age = 23
+hall = "STAH"
+#print(f"My name is {name}. I am {age} years old and I live in {hall} hall.") 
+
+x = 5 
+y = 10 
+
+a ,b, c = 5, 10, 15
+
+#print(c)
+
+#if x > y:
+  #print("x is greater than y")
+#else:
+  #print("x is less than y")
