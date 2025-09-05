c311a5549f353289d8e8b928cadeed80c1fd6a07
Erazul Islam <erazul89@gmail.com>
2025-09-06 02:10:57 +0600

    done

--- test.py
+++ test.py
@@ -14,3 +14,58 @@ a ,b, c = 5, 10, 15
   #print("x is greater than y")
 #else:
   #print("x is less than y")
+
+# fruits = ["apple", "banana", "cherry"]
+# #print(fruits[1])
+
+# # find even or odd number 
+
+# number = int(input("Enter a number: "))
+
+# if number % 2 == 0 :
+#     print(f"{number} is even number")
+# else : 
+#     print(f"{number} is odd number")
+
+
+# def add(a) :
+#     print("This is a function", a)
+# add()
+
+# x = 35e3
+
+# print(type(x))
+
+# print(10 == 9)
+
+# print(10 + 5)
+
+# i = 0
+# while i < 6:
+#   print(i)
+#   i += 1
+
+# i = 1
+# while i < 10:
+#   print(i)
+#   if i == 7:
+#     break
+#   i += 1
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+
+# with open("demofile3.txt", "a") as f:
+#   f.write("This is a python file")
+
+# #open and read the file after the appending:
+# with open("demofile.txt") as f:
+#   print(f.read())
+
+# f = open("demofile.txt")
+# print(f.read())
+
+# import os
+
+# os.remove("demofile3.txt")
