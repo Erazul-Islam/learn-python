15f7e7b08b36d36f01147f5ef52c14ed170c2fd4
Erazul Islam <erazul89@gmail.com>
2025-09-07 23:57:17 +0600

    added

--- forloop.py
+++ forloop.py
@@ -3,5 +3,21 @@
 # for x in fruits:
 #   print(x)
 
-for x in range(6, 61, 6):
-  print(x)
+# for x in range(6, 61, 6):
+#   print(x)
+
+# s = "DoctorPhenomenal"
+# y = "Geeks"
+
+# # output = DcoPeoea
+
+# # result = ""
+
+# # for i in range (0, len(y), 2 ):
+# #     result += y[i]
+
+# # print(result)
+
+# result = y[::2]
+
+# print(result)
---
+++ while.py
@@ -0,0 +1,11 @@
+# i = 1 
+
+# while i <= 6 : 
+#     print(i)
+#     i += 1
+
+# x = 3 
+
+# while x >= 0 :
+#     print(x, end=" ")
+#     x -= 1
